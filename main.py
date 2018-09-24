2ab99a92f2d94acfce603399fe780a76ee5eb7e4
Max <max@MacBook-Pro-de-Max.local>
2018-09-24 10:17:48 +0200

    Introduction comment

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Maximilien
+Romain
+2018
+
+This software is a simple program that helps the user to train reading music sheet
+
+Do  -   Re  -   Mi   -  Fa   -  Sol  -  La   -  Si   -  Do
+1       2       3       4       5       6       7       8
+
+***** Sol key *****
+                     Sol
+-------------------Fa--------------------
+                 Mi
+---------------Re------------------------
+             Do
+-----------Si----------------------------
+         La
+------Sol--------------------------------
+    Fa
+--Mi-------------------------------------
+RE
+
+
+"""
